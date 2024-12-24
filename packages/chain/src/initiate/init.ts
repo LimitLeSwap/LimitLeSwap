571fb28cee33bf2526444cdfc8467f428967671c
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2024-12-24 16:35:14 +0300

    add more more trade in init

--- packages/chain/src/initiate/init.ts
+++ packages/chain/src/initiate/init.ts
@@ -3,36 +3,39 @@ import { Balance, UInt64 as PUInt64, TokenId } from "@proto-kit/library";
 import { client as appChain } from "../environments/client.config";
 import { InMemorySigner } from "@proto-kit/sdk";
 import { Balances } from "../runtime/modules/balances";
+import { Route } from "../runtime/utils/route";
 
-const startClient = async () => {
-    // you are seeing this because I was too lazy to use .env. steal this key if you want :p
-    const publisherKey = PrivateKey.fromBase58(
-        "EKEot89cv2Uq217QaeePEJwoXyVdck7haEwFqJxoSKXmvUntfpTw"
-    );
-    const publisher = publisherKey.toPublicKey();
+let tokens = [];
 
-    let balances: Balances;
+// you are seeing this because I was too lazy to use .env. steal this key if you want :p
+const publisherKey = PrivateKey.fromBase58("EKEot89cv2Uq217QaeePEJwoXyVdck7haEwFqJxoSKXmvUntfpTw");
+const publisher = publisherKey.toPublicKey();
 
-    const signer = PrivateKey.random();
-    const sender = signer.toPublicKey();
+const signer = PrivateKey.random();
+const sender = signer.toPublicKey();
 
-    const wallet = PublicKey.fromBase58("B62qnBa7PPM4VrE3LqXx7HjDWPDTs1ePdV7hwwuSkmT31Lrr1CQpJna");
+const wallet = PublicKey.fromBase58("B62qnBa7PPM4VrE3LqXx7HjDWPDTs1ePdV7hwwuSkmT31Lrr1CQpJna");
 
-    await appChain.start();
-    const inMemorySigner = new InMemorySigner();
+let nonce = 0;
 
-    appChain.registerValue({
-        Signer: inMemorySigner,
-    });
+await appChain.start();
+const inMemorySigner = new InMemorySigner();
 
-    const resolvedInMemorySigner = appChain.resolve("Signer") as InMemorySigner;
-    resolvedInMemorySigner.config = { signer };
+appChain.registerValue({
+    Signer: inMemorySigner,
+});
 
+const resolvedInMemorySigner = appChain.resolve("Signer") as InMemorySigner;
+resolvedInMemorySigner.config = { signer };
+
+const balances = appChain.runtime.resolve("Balances");
+const orderbook = appChain.runtime.resolve("OrderBook");
+const pool = appChain.runtime.resolve("PoolModule");
+const router = appChain.runtime.resolve("RouterModule");
+
+const initEnv = async () => {
     console.log(publisher.toBase58());
 
-    let tokens = [];
-
-    balances = appChain.runtime.resolve("Balances");
     for (let i = 0; i < 4; i++) {
         try {
             const tokenId = TokenId.random();
@@ -42,7 +45,7 @@ const startClient = async () => {
                 await balances.createToken(tokenId);
             });
 
-            tx.transaction!.nonce = UInt64.from(3 * i);
+            tx.transaction!.nonce = UInt64.from(nonce++);
             tx.transaction = tx.transaction?.sign(publisherKey);
             await tx.send();
 
@@ -50,7 +53,7 @@ const startClient = async () => {
                 await balances.mintToken(tokenId, wallet, PUInt64.from(1000000000));
             });
 
-            mintTx.transaction!.nonce = UInt64.from(3 * i + 1);
+            mintTx.transaction!.nonce = UInt64.from(nonce++);
             mintTx.transaction = mintTx.transaction?.sign(publisherKey);
             await mintTx.send();
 
@@ -58,7 +61,7 @@ const startClient = async () => {
                 await balances.mintToken(tokenId, publisher, PUInt64.from(1000000000));
             });
 
-            mintTx.transaction!.nonce = UInt64.from(3 * i + 2);
+            mintTx.transaction!.nonce = UInt64.from(nonce++);
             mintTx.transaction = mintTx.transaction?.sign(publisherKey);
             await mintTx.send();
         } catch (e) {
@@ -66,8 +69,6 @@ const startClient = async () => {
         }
     }
 
-    const orderbook = appChain.runtime.resolve("OrderBook");
-    const pool = appChain.runtime.resolve("PoolModule");
     const tokenIn = await balances.tokens.get(Field.from(0));
     const tokenOut = await balances.tokens.get(Field.from(1));
     const amountIn = Balance.from(100);
@@ -78,7 +79,7 @@ const startClient = async () => {
         await balances.mintToken(tokenIn.value, publisher, PUInt64.from(100));
     });
 
-    tx.transaction!.nonce = UInt64.from(12);
+    tx.transaction!.nonce = UInt64.from(nonce++);
     tx.transaction = tx.transaction?.sign(publisherKey);
     await tx.send();
     tx = await appChain.transaction(publisher, async () => {
@@ -91,7 +92,7 @@ const startClient = async () => {
         );
     });
     console.log("Creating limit order");
-    tx.transaction!.nonce = UInt64.from(13);
+    tx.transaction!.nonce = UInt64.from(nonce++);
     tx.transaction = tx.transaction?.sign(publisherKey);
     await tx.send();
 
@@ -99,7 +100,7 @@ const startClient = async () => {
         await orderbook.cancelLimitOrder(Field.from(0));
     });
     console.log("Canceling limit order");
-    tx.transaction!.nonce = UInt64.from(14);
+    tx.transaction!.nonce = UInt64.from(nonce++);
     tx.transaction = tx.transaction?.sign(publisherKey);
     await tx.send();
 
@@ -115,7 +116,7 @@ const startClient = async () => {
         );
     });
     console.log("Creating pool");
-    tx.transaction!.nonce = UInt64.from(15);
+    tx.transaction!.nonce = UInt64.from(nonce++);
     tx.transaction = tx.transaction?.sign(publisherKey);
     await tx.send();
 
@@ -131,10 +132,24 @@ const startClient = async () => {
         );
     });
     console.log("Creating pool 2");
-    tx.transaction!.nonce = UInt64.from(16);
+    tx.transaction!.nonce = UInt64.from(nonce++);
     tx.transaction = tx.transaction?.sign(publisherKey);
     await tx.send();
 };
 
-await startClient();
+const testTransactions = async () => {
+    console.log("Testing trade route");
+    const emptyRoute = Route.empty();
+
+    const tx = await appChain.transaction(publisher, async () => {
+        await router.tradeRoute(emptyRoute);
+    });
+
+    tx.transaction!.nonce = UInt64.from(nonce++);
+    tx.transaction = tx.transaction?.sign(publisherKey);
+    await tx.send();
+};
+
+await initEnv();
+await testTransactions();
 process.exit(0);
