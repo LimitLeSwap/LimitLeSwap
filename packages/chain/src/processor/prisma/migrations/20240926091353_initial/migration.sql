946b8720a653dc0307eeb2c669b490ad71519c21
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2024-12-24 16:34:52 +0300

    implement blockcandle schema

--- packages/chain/src/processor/prisma/migrations/20240926091353_initial/migration.sql
+++ packages/chain/src/processor/prisma/migrations/20240926091353_initial/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable Block
 CREATE TABLE "Block" (
     "height" INTEGER NOT NULL,
+    "timestamp" TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT "Block_pkey" PRIMARY KEY ("height")
 );
 
@@ -10,6 +11,7 @@ CREATE TABLE "Token" (
     "decimals" INTEGER NOT NULL,
     "totalSupply" BIGINT NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT "Token_pkey" PRIMARY KEY ("tokenId")
 );
 
@@ -24,12 +26,10 @@ CREATE TABLE "Balance" (
     "address" TEXT NOT NULL,
     "amount" BIGINT NOT NULL,
     CONSTRAINT "Balance_pkey" PRIMARY KEY ("height","address","tokenId")
-    -- CONSTRAINT "Balance_block_fkey" FOREIGN KEY ("height") REFERENCES "Block" ("height") ON DELETE CASCADE ON UPDATE CASCADE,
-    -- CONSTRAINT "Balance_token_fkey" FOREIGN KEY ("tokenId") REFERENCES "Token" ("tokenId") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- CREATE INDEX "Balance_address_idx" ON "Balance" ("address");
--- CREATE INDEX "Balance_tokenId_idx" ON "Balance" ("tokenId");
+CREATE INDEX "Balance_tokenId_idx" ON "Balance" ("tokenId");
+CREATE INDEX "Balance_address_idx" ON "Balance" ("address");
 
 -- CreateTable Pool
 CREATE TABLE "Pool" (
@@ -41,6 +41,7 @@ CREATE TABLE "Pool" (
     "totalLpAmount" BIGINT NOT NULL,
     "feePercentage" INTEGER NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT "Pool_pkey" PRIMARY KEY ("poolId")
 );
 
@@ -57,8 +58,6 @@ CREATE TABLE "Swap" (
     "owner" TEXT NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT "Swap_pkey" PRIMARY KEY ("swapId")
-    -- CONSTRAINT "Swap_pool_fkey" FOREIGN KEY ("poolId") REFERENCES "Pool" ("poolId") ON DELETE CASCADE ON UPDATE CASCADE,
-    -- CONSTRAINT "Swap_block_fkey" FOREIGN KEY ("blockHeight") REFERENCES "Block" ("height") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable LimitOrder
@@ -74,8 +73,8 @@ CREATE TABLE "LimitOrder" (
     "active" BOOLEAN NOT NULL,
     "expireBlock" INTEGER NOT NULL,
     "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT "LimitOrder_pkey" PRIMARY KEY ("orderId")
-    -- CONSTRAINT "LimitOrder_pool_fkey" FOREIGN KEY ("poolId") REFERENCES "Pool" ("poolId") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE INDEX "LimitOrder_active_idx" ON "LimitOrder" ("active");
@@ -85,16 +84,32 @@ CREATE INDEX "LimitOrder_token0Price_token1Price_idx" ON "LimitOrder" ("token0Pr
 CREATE INDEX "LimitOrder_active_poolId_idx" ON "LimitOrder" ("active", "poolId");
 CREATE INDEX "LimitOrder_owner_active_idx" ON "LimitOrder" ("owner", "active");
 
--- CreateTable TokenTrade
-CREATE TABLE "TokenTrade" (
-    "tradeId" TEXT NOT NULL,
-    "poolId" TEXT NOT NULL,
-    "token0Amount" BIGINT NOT NULL,
-    "token1Amount" BIGINT NOT NULL,
-    "token0Price" NUMERIC(38, 18) NOT NULL,
-    "token1Price" NUMERIC(38, 18) NOT NULL,
-    "token0In1Out" BOOLEAN NOT NULL,
-    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT "TokenTrade_pkey" PRIMARY KEY ("tradeId")
-    -- CONSTRAINT "TokenTrade_pool_fkey" FOREIGN KEY ("poolId") REFERENCES "Pool" ("poolId") ON DELETE CASCADE ON UPDATE CASCADE
+-- Create block-based candle table
+CREATE TABLE "BlockCandle" (
+  "id" TEXT NOT NULL,
+  "blockHeight" INT NOT NULL,
+  "poolId" TEXT NOT NULL,
+
+  "token0Id" TEXT NOT NULL,
+  "token1Id" TEXT NOT NULL,
+
+  "openT0" NUMERIC(38, 18) NOT NULL,
+  "highT0" NUMERIC(38, 18) NOT NULL,
+  "lowT0" NUMERIC(38, 18) NOT NULL,
+  "closeT0" NUMERIC(38, 18) NOT NULL,
+  "volumeT0" BIGINT NOT NULL,
+
+  "openT1" NUMERIC(38, 18) NOT NULL,
+  "highT1" NUMERIC(38, 18) NOT NULL,
+  "lowT1" NUMERIC(38, 18) NOT NULL,
+  "closeT1" NUMERIC(38, 18) NOT NULL,
+  "volumeT1" BIGINT NOT NULL,
+
+  "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+  "updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+  CONSTRAINT "BlockCandle_pkey" PRIMARY KEY ("id")
 );
+
+CREATE INDEX "BlockCandle_blockHeight_idx" ON "BlockCandle" ("blockHeight");
+CREATE INDEX "BlockCandle_poolId_idx" ON "BlockCandle" ("poolId");
+CREATE INDEX "BlockCandle_token0Id_token1Id_idx" ON "BlockCandle" ("token0Id", "token1Id"); 
